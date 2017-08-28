ac3510134fc8818ae0fe1dc9fe7a31ad20d3f91d
Luís Ferreira <lsferreira169@gmail.com>
2017-08-28 10:56:35 +0100

    Import code

---
+++ app.cmake
@@ -0,0 +1,9 @@
+message(STATUS "Loading tests-tlib module...")
+
+if (NOT CONFIGURED_ONCE)
+	set(APP_TESTS_TLIB_SOURCE_DIR ${APP_TESTS_TLIB_DIR}/src)
+endif()
+
+add_executable(aurorafw_app_tests_string ${APP_TESTS_TLIB_SOURCE_DIR}/test_string.cpp)
+
+set_target_properties(aurorafw_app_tests_string PROPERTIES OUTPUT_NAME test_string.out)

99a610af63940375c04edf29009431e92c56b286
Luís Ferreira <lsferreira169@gmail.com>
2017-08-28 16:43:54 +0100

    Fix app and library link

--- app.cmake
+++ app.cmake
@@ -6,4 +6,6 @@ endif()
 
 add_executable(aurorafw_app_tests_string ${APP_TESTS_TLIB_SOURCE_DIR}/test_string.cpp)
 
+target_link_libraries(aurorafw_app_tests_string aurorafw-core aurorafw-tlib aurorafw-cli)
+
 set_target_properties(aurorafw_app_tests_string PROPERTIES OUTPUT_NAME test_string.out)

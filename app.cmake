12ea60f13f0b937a79247534fc50dec35fdb1c9b
Luís Ferreira <lsferreira169@gmail.com>
2017-09-03 00:25:00 +0100

    Making several code optimizations and project restructuration

--- app.cmake
+++ app.cmake
@@ -1,11 +1,27 @@
+# ┌─┐┬ ┬┬─┐┌─┐┬─┐┌─┐  ┌─┐┬─┐┌─┐┌┬┐┌─┐┬ ┬┌─┐┬─┐┬┌─
+# ├─┤│ │├┬┘│ │├┬┘├─┤  ├┤ ├┬┘├─┤│││├┤ ││││ │├┬┘├┴┐
+# ┴ ┴└─┘┴└─└─┘┴└─┴ ┴  └  ┴└─┴ ┴┴ ┴└─┘└┴┘└─┘┴└─┴ ┴
+# A Powerful General Purpose Framework
+# More information in: https://aurora-fw.github.io/
+#
+# Copyright (C) 2017 Aurora Framework, All rights reserved.
+#
+# This file is part of the Aurora Framework. This framework is free
+# software; you can redistribute it and/or modify it under the terms of
+# the GNU General Public License version 3 as published by the Free
+# Software Foundation and appearing in the file LICENSE included in the
+# packaging of this file. Please review the following information to
+# ensure the GNU General Public License version 3 requirements will be
+# met: https://www.gnu.org/licenses/gpl-3.0.html.
+
 message(STATUS "Loading tests-tlib app...")
 
 if (NOT CONFIGURED_ONCE)
-	set(APP_TESTS_TLIB_SOURCE_DIR ${APP_TESTS_TLIB_DIR}/src)
+	set(AURORAFW_APP_TESTS_TLIB_SOURCE_DIR ${AURORAFW_APP_TESTS_TLIB_DIR}/src)
 endif()
 
-add_executable(aurorafw_app_tests_string ${APP_TESTS_TLIB_SOURCE_DIR}/test_string.cpp)
+add_executable(aurorafw_app_tests_string ${AURORAFW_APP_TESTS_TLIB_SOURCE_DIR}/main.cpp)
 
 target_link_libraries(aurorafw_app_tests_string aurorafw-core aurorafw-tlib aurorafw-cli)
 
-set_target_properties(aurorafw_app_tests_string PROPERTIES OUTPUT_NAME test_string.out)
+set_target_properties(aurorafw_app_tests_string PROPERTIES OUTPUT_NAME aurorafw_app_tests_string.out)
